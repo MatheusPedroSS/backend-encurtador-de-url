7f8783b8bf80928cf4a21253cc2f44b28864cea2
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-04-09 18:32:16 -0300

    Commit inicial - Criando o projeto

---
+++ src/main/java/com/pedro/backendencurtadordeurl/BackendEncurtadorDeUrlApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.backendencurtadordeurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEncurtadorDeUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEncurtadorDeUrlApplication.class, args);
+	}
+
+}
